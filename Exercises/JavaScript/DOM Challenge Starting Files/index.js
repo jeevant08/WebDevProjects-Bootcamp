4b93f04fa8fd359d39ba0b22e213512ab6175b64
Jeevan <ugginter@gmail.com>
2026-03-07 18:28:22 +0530

    Basics of JavaScript and DOM

---
+++ Exercises/JavaScript/DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+var li=document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild;
+li.innerHTML="Jeevan";
+
+var an=document.querySelector(".list a");
+an.style.color="green";
+
+document.querySelector("button").style.backgroundColor="yellow";
+
+document.querySelector("h1").classList.add("huge");
